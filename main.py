9953efb37b42af07230817c1cc1b24a3538da825
nikBaben <faymen44@mail.ru>
2022-12-17 13:56:42 +0300

    замена экрана главного меню

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def menu():
         t = video.current_time.format("%h:%m:%s")
         if t == "00:02:26":
             video.restart()
-        screen.blit(backi, (0, 0))
+       # screen.blit(backi, (0, 0))
         a = menu1.convert_alpha()
         screen.blit(a, (150, 100))
         # screen.blit(text_logo, (200, 200))
